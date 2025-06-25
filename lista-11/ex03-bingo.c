160fd3cf656d3c58d7425f0bc11c37f3957f9104
brunoconcli <bruno.concli@becomex.com.br>
2025-06-25 18:37:39 -0300

    refactor: Alterado tipo do ponteiro usado de char para int

--- lista-11/ex03-bingo.c
+++ lista-11/ex03-bingo.c
@@ -2,47 +2,44 @@
 #include<stdlib.h>
 #include<time.h>
 
-char** alocarMatrizChar(int linhas, int colunas)
+int** alocarMatrizInt(int linhas, int colunas)
 {
-	char** matriz = (char**)malloc(linhas*sizeof(char*));
+	int** matriz = (int**)malloc(linhas*sizeof(int*));
 	for (int i = 0; i< linhas; i++)
 	{
-		matriz[i] = (char*)malloc(colunas*sizeof(char));
+		matriz[i] = (int*)malloc(colunas*sizeof(int));
 	}
 	return matriz;
 }
 
-char* gerarColunaAleatoria(int minimo, int maximo, int tamanho)
+int* gerarColunaAleatoria(int minimo, int maximo, int tamanho)
 {
 	srand(time(NULL));
-	char* coluna = (char*) malloc(tamanho*sizeof(char));
-	char valor;
+	int* coluna = (int*) malloc(tamanho*sizeof(int));
+	int valor;
 	for (int i = 0; i < tamanho; i++)
 	{
-		valor = rand()%((maximo-minimo+1)+minimo) + '0';
+		valor = rand()%((maximo-minimo+1)+minimo);
 		coluna[i] = valor;
 	}
 	return coluna;
 }
 
-char** gerarCartela()
+int** gerarCartela()
 {
 	int minimo=1, maximo=15;
-	char** cartela = alocarMatrizChar(6, 5);
-	char* titulo[5] = {"B","I","N","G","O"};
-	char* coluna;
+	int* coluna;
+	int** cartela = alocarMatrizInt(5, 5);
+	char titulo[5] = {'B','I','N','G','O'};
 
-	for (int j = 0; j<5; j++)
-		cartela[0][j] = titulo[j][0];
-	
 	for (int i = 0; i<5; i++)
 	{
 		coluna = gerarColunaAleatoria(minimo, maximo, 5);
 		minimo+=15;
 		maximo+=15;
-		for (int j = 1; j < 6; j++)
+		for (int j = 0; j < 5; j++)
 		{
-			cartela[j][i] = coluna[i];
+			cartela[j][i] = coluna[j];
 		}
 	}
 	return cartela;
@@ -74,13 +71,13 @@ void imprimirCartela()
 	}
 }
 
-void imprimirMatriz(char** matriz, int linhas, int colunas)
+void imprimirMatriz(int** matriz, int linhas, int colunas)
 {
 	for (int i = 0; i<linhas; i++)
 	{
 		for (int j = 0; j<colunas; j++)
 		{
-			printf("%c ", matriz[i][j]);
+			printf("%d\t", matriz[i][j]);
 		}
 		printf("\n");
 	}
@@ -88,12 +85,8 @@ void imprimirMatriz(char** matriz, int linhas, int colunas)
 
 int main()
 {
-	// char** cartela = gerarCartela();
-	// imprimirMatriz(cartela, 6, 5);
-	// imprimirCartela();
-	char* coluna = gerarColunaAleatoria(16, 30, 5);
-	for (int i =0; i<5;i++)
-		printf("%c", coluna[i]);
+	int** cartela = gerarCartela();
+	imprimirMatriz(cartela, 5, 5);
 	return(0);
 }
 
