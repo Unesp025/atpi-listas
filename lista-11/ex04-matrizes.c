6c0f9d49364737275fd99cc0090d4c00766c4965
brunoconcli <bruno.concli@becomex.com.br>
2025-06-28 09:33:52 -0300

    feat: Obtida conversão de char para int funcional

--- lista-11/ex04-matrizes.c
+++ lista-11/ex04-matrizes.c
@@ -2,6 +2,17 @@
 #include<stdlib.h>
 #include<string.h>
 
+int **alocarMatriz(int linhas, int colunas)
+{
+    int **matriz = (int **) malloc(linhas * sizeof(int *));
+    for (int i = 0; i < linhas; i++)
+    {
+        int *linha = (int *) malloc(colunas * sizeof(int));
+        matriz[i] = linha;
+    }
+    return matriz;
+}
+
 int **obterMatrizDeArquivo(char caminho[100])
 {
     int linhas, colunas;
@@ -9,7 +20,7 @@ int **obterMatrizDeArquivo(char caminho[100])
     char linha[tamanhoLinha];
     char *celulaAtual;
     FILE *input = fopen(caminho, "r");
-    
+
     if (input != NULL)
     {
         fgets(linha, tamanhoLinha, input);
@@ -19,13 +30,41 @@ int **obterMatrizDeArquivo(char caminho[100])
         colunas = *celulaAtual - '0';
         printf("Linhas: %d | Colunas: %d\n", linhas, colunas);
         
+        int **matriz = alocarMatriz(linhas, colunas);
+        int i = 0, j = 0;
+        
         while (fgets(linha, tamanhoLinha, input))
         {
-            printf("%s", linha);
+            celulaAtual = strtok(linha, ",");
+            do
+            {
+                if (celulaAtual)
+                {
+                    printf("%d\t", atoi(celulaAtual));
+                    // matriz[i][j] = atoi(celulaAtual);
+                    celulaAtual = strtok(NULL, ",");
+                }
+                j++;
+            }
+            while(celulaAtual!= NULL);
+            printf("\n");
+            i++;
         }
         fclose(input); 
     }
     return NULL;
+}
+
+void imprimirMatriz(int **matriz, int linhas, int colunas)
+{
+    for (int i = 0; i < linhas; i++)
+    {
+        for (int j = 0; j < colunas; j++)
+        {
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int **somarMatrizes(int **A, int **B)
